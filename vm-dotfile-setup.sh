d373737bfff981654cce30b493286447f41cb62f
tahardi <Taylor.A.Hardin.GR@dartmouth.edu>
2019-03-20 19:38:19 -0400

    script for setting up vm dotfiles

---
+++ vm-dotfile-setup.sh
@@ -0,0 +1,3 @@
+cp /home/vagrant/cs50-shared/dotfiles/cs50-home/.bash_profile ~/
+cp /home/vagrant/cs50-shared/dotfiles/cs50-home/.bash_logout ~/
+cp /home/vagrant/cs50-shared/dotfiles/cs50-home/.bashrc ~/
