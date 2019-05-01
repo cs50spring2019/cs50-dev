e60759a1c6ee09f1699efe551096087e862ffc21
tahardi <Taylor.A.Hardin.GR@dartmouth.edu>
2019-05-01 09:32:36 -0400

    adding copy script for cs server setup

---
+++ setup/cs50-server-bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+echo "Installing dot files..."
+dotdir=~/cs50-dev/dotfiles/csservers
+for dot in $dotdir/*; do
+    dotfile=.${dot##*/}
+    dotpath=~/$dotfile
+    try "cp $dot $dotpath"
+done
+
+# Configure dotfiles
+git config --global core.excludesfile ~/.gitignore_global
+echo ""
+echo "bootstrap complete"
